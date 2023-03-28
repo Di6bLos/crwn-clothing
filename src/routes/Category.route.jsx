fbfa651e1de738c096f675d96dfb502f29596c7d
CarlosPrieto <carlosprieto66@icloud.com>
2023-03-28 11:28:12 -0700

    fixed single-category page styling

--- src/routes/Category.route.jsx
+++ src/routes/Category.route.jsx
@@ -1,24 +1,29 @@
 import { useParams } from "react-router-dom";
-import { useContext, useState, useEffect } from "react";
+import { useContext, useState, useEffect, Fragment } from "react";
 import { CategoriesContext } from "../contexts/Categories.context";
 
 import ProductCard from "../components/ProductCard";
 
 const Category = () => {
-    const { category } = useParams();
-    const {categories } = useContext(CategoriesContext);
-    const [products, setProducts] = useState(categories[category]);
+  const { category } = useParams();
+  const { categories } = useContext(CategoriesContext);
+  const [products, setProducts] = useState(categories[category]);
 
-    useEffect(()=> {
-        setProducts(categories[category]);
-    }, [category, categories])
+  useEffect(() => {
+    setProducts(categories[category]);
+  }, [category, categories]);
 
-    return (
-        <div className="category-container">
-           {products &&
-            products.map((product) => <ProductCard key={product.id} product={product} />)}
-        </div>
-    )
-}
+  return (
+    <Fragment>
+      <h2>{category.toUpperCase()}</h2>
+      <div className="single-category-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </Fragment>
+  );
+};
 
 export default Category;
--- src/routes/Checkout.route.jsx
+++ src/routes/Checkout.route.jsx
@@ -1,28 +1,14 @@
 import { useContext } from "react";
 import { CartContext } from "../contexts/Cart.context";
 import CheckoutItem from "../components/CheckoutItem";
+import CheckoutHeader from "../components/CheckoutHeader";
+
 
 const Checkout = () => {
   const { cartItems, cartTotal } = useContext(CartContext);
   return (
     <div className="checkout-container">
-      <div className="checkout-header">
-        <div className="header-block">
-          <span>Product</span>
-        </div>
-        <div className="header-block">
-          <span>Description</span>
-        </div>
-        <div className="header-block">
-          <span>Quantity</span>
-        </div>
-        <div className="header-block">
-          <span>Price</span>
-        </div>
-        <div className="header-block">
-          <span>Remove</span>
-        </div>
-      </div>
+      <CheckoutHeader />
       {cartItems.map((item) => (
         <CheckoutItem key={item.id} item={item} />
       ))}
